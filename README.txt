f5560cd2d4f12215aad550129ba467c74030fbe9
RemiLecouillard <remi@lecouillard.org>
2018-12-08 17:38:37 +0100

    First Commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+This program is an ENSICAEN's students project.
+It deals with image parsing.
---
+++ src/main/helloWorld.c
@@ -0,0 +1,17 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello World !");
+
+    return 0;
+}
